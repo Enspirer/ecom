b4113b19cf927a957f4f91f8901a69e18f2327f7
Zajjith Vedha <zajjith@gmail.com>
2021-09-22 13:08:05 +0530

    index page

--- resources/views/frontend/inc/footer.blade.php
+++ resources/views/frontend/inc/footer.blade.php
@@ -1,8 +1,73 @@
-<div class="footer">
-    <div class="row">
-        <div class="col-4">
-            <img src="{{ url('logo.png') }}" alt="" class="img-fluid">
-            <p>We are a team of designers and developers that create high quality </p>
+<div class="container-fluid footer px-0" style="background-color: #000000;">
+    <div class="container text-white" style="padding-top: 4rem; padding-bottom: 0.7rem;">
+        <div class="row">
+            <div class="col-3">
+                <img src="{{ url('footer_logo.png') }}" alt="" class="img-fluid mb-2">
+                <p class="mb-3">We are a team of designers and developers that create high quality Magento, Prestashop, Opencart.</p>
+                
+                <div class="row mb-2">
+                    <div class="col-1">
+                        <i class="fas fa-phone-alt"></i>
+                    </div>
+                    <div class="col-10">
+                        <p>+94 777000000</p>
+                    </div>
+                </div>
+
+                <div class="row mb-2">
+                    <div class="col-1">
+                        <i class="far fa-envelope"></i>
+                    </div>
+                    <div class="col-10">
+                        <p>info@eshop.com</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-1">
+                        <i class="fas fa-map-marker-alt"></i>
+                    </div>
+                    <div class="col-10">
+                        <p>587/C Nugegoda, <br> Colombo 5</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col-3 pt-4">
+                <h5 class="mb-3">Customer Support</h5>
+
+                <a href="#" class="text-decoration-none text-white d-inline-block mb-3">Products</a> <br>
+                <a href="#" class="text-decoration-none text-white d-inline-block mb-3">What's New</a> <br>
+                <a href="#" class="text-decoration-none text-white d-inline-block mb-3">Contact Us</a> <br>
+            </div>
+
+            <div class="col-3 pt-4">
+                <h5 class="mb-3">Customer Support</h5>
+
+                <a href="#" class="text-decoration-none text-white d-inline-block mb-3">Products</a> <br>
+                <a href="#" class="text-decoration-none text-white d-inline-block mb-3">What's New</a> <br>
+                <a href="#" class="text-decoration-none text-white d-inline-block mb-3">Contact Us</a> <br>
+            </div>
+
+            <div class="col-3 pt-4">
+                <h5 class="mb-3">Follow Us</h5>
+
+                <a href="#" class="text-decoration-none text-white d-inline-block mr-3"><i class="fab fa-facebook-square" style="font-size: 2.5rem;"></i></a>
+                <a href="#" class="text-decoration-none text-white d-inline-block mr-3"><i class="fab fa-instagram-square" style="font-size: 2.5rem;"></i></a>
+                <a href="#" class="text-decoration-none text-white d-inline-block mr-3"><i class="fab fa-linkedin" style="font-size: 2.5rem;"></i></a>
+                <a href="#" class="text-decoration-none text-white d-inline-block mr-3"><i class="fab fa-youtube" style="font-size: 2.5rem;"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <hr class="mb-2" style="border-color: #EEEEEE!important;">
+
+    <div class="container">
+        <div class="row justify-content-end mb-2">
+            <div class="col-3 text-right">
+                <p class="text-white">Powered by <a href="https:://www.enspirer.com" class=>Enspirer</a></p>
+            </div>
         </div>
     </div>
 </div>
